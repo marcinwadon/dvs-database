13c23823cfcbc63f9df25e8221879d59f248e648
Marcin Wadon <marcin@wadon.net>
2016-09-28 09:48:12 +0200

    Add open database method

--- test/Database.spec.ts
+++ test/Database.spec.ts
@@ -1,13 +1,31 @@
 /// <reference path="../typings/chai/chai.d.ts" />
 /// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/sinon/sinon.d.ts" />
+/// <reference path="../typings/sinon-chai/sinon-chai.d.ts" />
 
 
 import Database from '../lib/Database';
-import { expect } from 'chai';
+import { expect, should } from 'chai';
+import * as sinon from 'sinon';
 
 describe('Database', () => {
+  let database: Database;
+
+  beforeEach(() => {
+    database = new Database();
+  });
+
   it('should be initialized', () => {
-    const database: Database = new Database();
     expect(database).to.be.instanceof(Database);
+  });
+
+  it('should allow to open database', () => {
+    sinon.spy(database, 'openDb');
+
+    expect(database.openDb).to.not.have.been.called;
+
+    database.openDb();
+
+    expect(database.openDb).to.have.been.called;
   });
 });
